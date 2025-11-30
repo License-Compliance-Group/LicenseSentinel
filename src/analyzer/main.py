7c8bac9b0322bd94df28b37c8d2061be32e61c9e
kbublij2 <k.bublij@studenti.unisannio.it>
2025-11-30 16:20:17 +0100

    Undelete main.py

---
+++ src/analyzer/main.py
@@ -0,0 +1,17 @@
+import os
+from analyzer import package_metadata_fetcher
+
+
+print("Working directory:", os.getcwd())
+print("File esiste?:", os.path.exists("requirements.txt"))
+
+
+def main():
+    file_path = "requirements.txt"
+    finder = package_metadata_fetcher.build_package_metadata(file_path)
+    # for pkg in finder:
+    #    print(f"{pkg.package} | {pkg.license_type} | {pkg.link}")
+
+
+if __name__ == "__main__":
+    main()
