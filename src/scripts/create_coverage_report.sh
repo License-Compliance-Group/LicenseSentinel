e00bdccebf59479bead1a620f7ba480726bf60f9
kbublij2 <k.bublij@studenti.unisannio.it>
2025-12-07 14:27:42 +0100

    Create a coverage report Bash script

---
+++ src/scripts/create_coverage_report.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This is intended to run as a GitHub action, hence Bash
+# If you're on Windows, your default Git installation should come with Bash
+
+cwd=$(pwd)
+echo "Working in folder $cwd"
+
+branch=$(git branch --show-current)
+echo "Working on branch $branch"
+
+echo "Executing tests..."
+coverage run -m coverage discover
+echo "Generating coverage report..."
+if [ ! -d "test/reports" ]; then
+    echo "Target directory does not exist, is the project root correct?"
+    exit 1
+fi
+
+coverage xml -o test/reports/coverage.xml
+
+if [ ! -e "test/reports/coverage.xml" ]; then
+    echo "Failed to create report file!"
+    exit 2
+fi
+
+exit 0
--- test/unittest/analyzer/test_license_compatibility_analyzer.py
+++ test/unittest/analyzer/test_license_compatibility_analyzer.py
@@ -1,8 +1,9 @@
 """Unit tests for LicenseCompatibilityAnalyzer"""
+import unittest
 from src.analyzer.license_compatibility_analyzer import LicenseCompatibilityAnalyzer
 
 
-class TestLicenseCompatibilityAnalyzerClass():
+class TestLicenseCompatibilityAnalyzerClass(unittest.TestCase):
     """Unit tests for LicenseCompatibilityAnalyzer class."""
     def test_analyzer_builds(self):
         """Ensure default config of LicenseCompatibilityAnalyzer works."""
